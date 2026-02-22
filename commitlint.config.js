64fc07fdba0831a27942feba5b5ce2c5c33e82c4
fluffiea <fluffiea@outlook.com>
2026-02-22 17:13:11 +0800

    ci: 构建github actions

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']],
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
     'subject-empty': [2, 'never']
